12ff0beec64a43280b37cc38a786acb6e0ec46d5
ameykulkarni03 <112329132+ameykulkarni03@users.noreply.github.com>
2024-01-11 01:03:24 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -20,8 +20,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function fetchMetaTags() {
     const metaTags = document.getElementsByTagName('meta');
-    return Array.from(metaTags).map(tag => ({
+    const desiredTags = ['title', 'description', 'keywords', 'viewport', 'robots'];
+    const imageRelatedTags = ['og:image', 'twitter:image'];
+
+    const filteredTags = Array.from(metaTags).filter(tag => {
+        const tagName = tag.getAttribute('name') || tag.getAttribute('property');
+        return (desiredTags.includes(tagName) || (tagName && tagName.startsWith('og:'))) && !imageRelatedTags.includes(tagName);
+    });
+
+    return filteredTags.map(tag => ({
         name: tag.getAttribute('name') || tag.getAttribute('property') || 'Unnamed Tag',
         content: tag.getAttribute('content') || 'No Content'
     }));
 }
+
+
